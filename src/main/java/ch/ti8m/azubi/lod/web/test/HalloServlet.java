729aa620c0171d57875f4fa7453db13035138e3f
Daniel Löffeler <daniel.loeffler@ti8m.ch>
2018-08-10 10:23:06 +0200

    Initial commit

---
+++ src/main/java/ch/ti8m/azubi/lod/web/test/HalloServlet.java
@@ -0,0 +1,27 @@
+package ch.ti8m.azubi.lod.web.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+@WebServlet("/hello")
+
+public class HalloServlet extends HttpServlet{
+    @Override
+    protected void doGet(httpServletRequest req, httpServletResponse resp)
+        throws IOException {
+        PrintWriter writer=new PrintWriter(resp.getWriter());
+
+        writer.write("<html>");
+        writer.write("<head>");
+        writer.write("<title>Example Servlet</title>");
+        writer.write("</head>");
+        writer.write("<link rel=\"stylesheet\" href=\"style.css\">");
+        writer.write("<body>");
+        writer.write("<h1>Hello Servlet</h1>");
+        writer.write("Current time is: "+LocalDateTime.now());
+        writer.write("</body>");
+        writer.write("</html>");
+
+    }
+}
---
+++ src/main/java/ch/ti8m/azubi/lod/web/test/Test.java
@@ -0,0 +1,13 @@
+package ch.ti8m.azubi.lod.web.test;
+
+import java.time.LocalDateTime;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello: "+LocalDateTime.now());
+
+        System.out.println("done");
+    }
+}
